153beb9706ccf39b323d4ce713e2516bdc8ebcc4
ORE01 <60872684+ORE01@users.noreply.github.com>
2024-04-17 10:59:11 +0200

    Add files via upload

--- r_tab/ISSUER.js
+++ r_tab/ISSUER.js
@@ -1,30 +1,27 @@
 import processData from '../renderer/dataProcessor.js';
 import { handleFormAction } from '../renderer/FormButtonHandler.js';
 import { filterColumnsInData } from '../renderer/dataProcessor.js';
+import { AppState } from '../AppState.js';
 
 let issuerData;
 let filteredIssuerData;
 
-// Function to filter ProdAllData based on filtersConfig
-function filterIssuerData(data, filtersConfig) {
-  return data.filter(dataPoint => {
-    return Object.entries(filtersConfig).every(([key, valueSet]) => {
-      return valueSet.has('ALL') || valueSet.has(dataPoint[key]);
-    });
-  });
-}
 
-export function handleIssuerData(receivedData, filtersConfig) {
+export function handleIssuerData(receivedData, appState) {
   const issuerDataContainer = document.getElementById('issuerDataContainer');
   issuerData = receivedData;
-  console.log('handleIssuerData:', issuerData); 
-
+  // issuerData  = AppState.filteredData['issuer'];
+   console.log('issuerData:', issuerData); 
+   //console.log('filtersConfig:', filtersConfig); 
   if (issuerDataContainer && issuerData) {
     let columns = ['ISSUER', 'TICKER', 'RATING'];
-    //let filteredProdData = filterColumnsInData((prodData), columns);
-    filteredIssuerData = filterColumnsInData(filterIssuerData(issuerData, filtersConfig), columns);
-    console.log('issuerData:', issuerData);
-    console.log('filteredIssuerData:', filteredIssuerData);
+    // filteredIssuerData  = issuerData// appState.getFilteredDataForTable(issuerData, 'issuer');
+    filteredIssuerData = appState.filteredData['issuer']
+    
+    // filteredIssuerData  = receivedData;
+    // console.log('issuerData:', issuerData);
+    
+    
     const issuerDataHTML = processData(filteredIssuerData, 'Issuer');
     issuerDataContainer.innerHTML = issuerDataHTML;
 
@@ -32,24 +29,30 @@ export function handleIssuerData(receivedData, filtersConfig) {
     const issuerEditButtons = document.querySelectorAll('#issuerDataContainer .edit-button');
     issuerEditButtons.forEach((button) => {
       button.addEventListener('click', (event) => {
+        console.log('filteredIssuerData:', filteredIssuerData);
         event.stopPropagation(); // Stop event propagation
         const tableName = 'Issuer';
         const actionType = 'edit';
         const rowIndex = parseInt(button.getAttribute('data-row'), 10);
-        handleFormAction(event, issuerData, rowIndex, tableName, actionType);
+
+        handleFormAction(event, filteredIssuerData, rowIndex, tableName, actionType);
       });
     });
 
     // Add Button
-    const issueraddButton = document.getElementById('issueraddButton');
+    const issueraddButton = document.getElementById('issuerAddButton');
     issueraddButton.addEventListener('click', (event) => {
       const tableName = 'Issuer';
       const actionType = 'add';
+      console.log('AddButton')
       handleFormAction(event, issuerData, null, tableName, actionType);
     });
   }
+
   
 }
+
+
 
 export { issuerData, filteredIssuerData };
 
