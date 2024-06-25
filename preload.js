6d3c4d24cf542a1ff8989442a72dc023192b0b1f
Wendler <thomas.wendler@uni-graz.at>
2024-06-25 15:30:20 +0200

    preload geändert, generatePDF neu

--- preload.js
+++ preload.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24966de370b41e3808a2c28dbb5ba54341ed67a5214e021d0763686287952517
-size 611
+oid sha256:364db75d45c6653c499606005add6d2ebbe5cd00b810dc4a331cac63515d209f
+size 746
