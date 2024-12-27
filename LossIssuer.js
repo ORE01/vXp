62e3e54fc2f665d8ce9e4ac3daa94a8543674fcb
ORE01 <ronald.schaufler@hotmail.com>
2024-12-27 09:58:34 +0100

    Add traffic light feature and update related files

--- LossIssuer.js
+++ LossIssuer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:474c6b8aa7719cdde7a9bf6b137df4b299e8ae465ada62968679a3343c43380d
-size 20430
+oid sha256:c150bbc1dacb469bb6b4ac60df62833e0412534739601abe869c43a28133f13f
+size 21546
---
+++ trafficLight.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:162139c5efdf3daa605c034d160231efa0b71516cd8b3c000dbb59212f82af10
+size 2348
